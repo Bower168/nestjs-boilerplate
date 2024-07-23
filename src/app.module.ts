744643fbb899af714531de0dce33909749f71fac
HQ-PS-LT-50211\DELL <limsopheak89@gmail.com>
2024-07-23 10:32:20 +0700

    chore: Add configuration module for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { validate } from './core/env/env.validation';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ validate })],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000).then(() => {
+    logger.debug(`Server is running on http://localhost:${process.env.PORT}`);
+  });
 }
 bootstrap();
