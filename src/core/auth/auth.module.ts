03a0e4fac3146c8bb5357eb56ad9f624f9c43c58
HQ-PS-LT-50211\DELL <limsopheak89@gmail.com>
2024-07-23 13:16:00 +0700

    chore: Update import path for DatabaseModule in auth.module.ts

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './service';
 import { userProviders } from 'src/entity/user/user.providers';
 import { JwtService } from '@nestjs/jwt';
-import { DatabaseModule } from '../TypeORM/typeorm.module';
+import { DatabaseModule } from '../typeorm/typeorm.module';
 
 @Module({
   imports: [DatabaseModule],
